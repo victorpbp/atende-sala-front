56c1fbe4d9f845c5fea7d94c0729b0276160244d
victorpbp <victorrpeixotoo@gmail.com>
2023-11-08 16:19:14 -0300

    Start for Relatorio Individual

---
+++ lib/reports/individual_report.dart
@@ -0,0 +1,114 @@
+import 'package:atende_sala/reports/report_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class RelatorioIndividual extends ConsumerWidget {
+  const RelatorioIndividual({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final relatorioIndividual = ref
+        .watch(listaRelatorioProvider)[ref.watch(relatorioSelecionadoProvider)];
+
+    return Scaffold(
+        appBar: AppBar(title: const Text('Relatório de Aula')),
+        body: Center(
+          child: SizedBox(
+            width: 300,
+            child: Container(
+                margin: const EdgeInsets.only(top: 40),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 20),
+                      child: Text(
+                        relatorioIndividual.nomeSala,
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(relatorioIndividual.data),
+                        Text(
+                            'Alunos: ${relatorioIndividual.info.quantidadeAlunosPresentes}/${relatorioIndividual.info.quantidadeAlunosMaximo}'),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'Duração: ${relatorioIndividual.info.duracaoMinutos} minutos',
+                        ),
+                        Text(
+                          'Tempo Médio de Atendimento: ${relatorioIndividual.info.tempoMedioAtendimento} minutos',
+                        ),
+                        Text(
+                          'Tamanho Máximo da Fila de Espera: ${relatorioIndividual.info.tamanhoMaximoFilaEspera} pessoas',
+                        ),
+                      ],
+                    ),
+                    DataTable(
+                      columns: const <DataColumn>[
+                        DataColumn(
+                          label: Expanded(
+                            child: Text(
+                              'Nome',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Expanded(
+                            child: Text(
+                              'Freq',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Expanded(
+                            child: Text(
+                              'T.Médio',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
+                          ),
+                        ),
+                      ],
+                      rows: const <DataRow>[
+                        DataRow(
+                          cells: <DataCell>[
+                            DataCell(Text('Alexandre Coelho')),
+                            DataCell(Text('6')),
+                            DataCell(Text('6.4 minutos')),
+                          ],
+                        ),
+                        DataRow(
+                          cells: <DataCell>[
+                            DataCell(Text('Glauber Gouveira')),
+                            DataCell(Text('2')),
+                            DataCell(Text('2.8 minutos')),
+                          ],
+                        ),
+                        DataRow(
+                          cells: <DataCell>[
+                            DataCell(Text('Lorena Roberta')),
+                            DataCell(Text('3')),
+                            DataCell(Text('4.2 minutos')),
+                          ],
+                        ),
+                        DataRow(
+                          cells: <DataCell>[
+                            DataCell(Text('Victor Peixoto')),
+                            DataCell(Text('5')),
+                            DataCell(Text('3.9 minutos')),
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                )),
+          ),
+        ));
+  }
+}
--- lib/reports/report_list.dart
+++ lib/reports/report_list.dart
@@ -1,68 +1,181 @@
+import 'package:atende_sala/reports/individual_report.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final listaRelatorioProvider = StateProvider<List<Relatorios>>((ref) => [
       Relatorios(
-          data: '01/01/2021',
-          nomeSala: 'Sala 1',
-          nomeProfessor: 'Professor 1',
+        data: '01/01/2021',
+        nomeSala: 'Sala 1',
+        nomeProfessor: 'Professor 1',
+        info: RelatorioInfo(
           quantidadeAlunosMaximo: 10,
-          quantidadeAlunosPresentes: 5),
+          quantidadeAlunosPresentes: 5,
+          duracaoMinutos: 60,
+          tempoMedioAtendimento: 10,
+          tamanhoMaximoFilaEspera: 5,
+          alunos: Alunos(
+            nome: 'Aluno 1',
+            quantidadeAtendimentos: 1,
+            tempoMedioAtendimento: 10,
+          ),
+        ),
+      ),
       Relatorios(
-          data: '02/01/2021',
-          nomeSala: 'Sala 2',
-          nomeProfessor: 'Professor 2',
+        data: '02/01/2021',
+        nomeSala: 'Sala 2',
+        nomeProfessor: 'Professor 2',
+        info: RelatorioInfo(
           quantidadeAlunosMaximo: 10,
-          quantidadeAlunosPresentes: 5),
+          quantidadeAlunosPresentes: 5,
+          duracaoMinutos: 60,
+          tempoMedioAtendimento: 10,
+          tamanhoMaximoFilaEspera: 5,
+          alunos: Alunos(
+            nome: 'Aluno 2',
+            quantidadeAtendimentos: 1,
+            tempoMedioAtendimento: 10,
+          ),
+        ),
+      ),
       Relatorios(
-          data: '03/01/2021',
-          nomeSala: 'Sala 3',
-          nomeProfessor: 'Professor 3',
+        data: '03/01/2021',
+        nomeSala: 'Sala 3',
+        nomeProfessor: 'Professor 3',
+        info: RelatorioInfo(
           quantidadeAlunosMaximo: 10,
-          quantidadeAlunosPresentes: 5),
+          quantidadeAlunosPresentes: 5,
+          duracaoMinutos: 60,
+          tempoMedioAtendimento: 10,
+          tamanhoMaximoFilaEspera: 5,
+          alunos: Alunos(
+            nome: 'Aluno 3',
+            quantidadeAtendimentos: 1,
+            tempoMedioAtendimento: 10,
+          ),
+        ),
+      ),
       Relatorios(
-          data: '04/01/2021',
-          nomeSala: 'Sala 4',
-          nomeProfessor: 'Professor 4',
+        data: '04/01/2021',
+        nomeSala: 'Sala 4',
+        nomeProfessor: 'Professor 4',
+        info: RelatorioInfo(
           quantidadeAlunosMaximo: 10,
-          quantidadeAlunosPresentes: 5),
+          quantidadeAlunosPresentes: 5,
+          duracaoMinutos: 60,
+          tempoMedioAtendimento: 10,
+          tamanhoMaximoFilaEspera: 5,
+          alunos: Alunos(
+            nome: 'Aluno 4',
+            quantidadeAtendimentos: 1,
+            tempoMedioAtendimento: 10,
+          ),
+        ),
+      ),
       Relatorios(
-          data: '05/01/2021',
-          nomeSala: 'Sala 5',
-          nomeProfessor: 'Professor 5',
+        data: '05/01/2021',
+        nomeSala: 'Sala 5',
+        nomeProfessor: 'Professor 5',
+        info: RelatorioInfo(
           quantidadeAlunosMaximo: 10,
-          quantidadeAlunosPresentes: 5),
+          quantidadeAlunosPresentes: 5,
+          duracaoMinutos: 60,
+          tempoMedioAtendimento: 10,
+          tamanhoMaximoFilaEspera: 5,
+          alunos: Alunos(
+            nome: 'Aluno 5',
+            quantidadeAtendimentos: 1,
+            tempoMedioAtendimento: 10,
+          ),
+        ),
+      ),
       Relatorios(
-          data: '06/01/2021',
-          nomeSala: 'Sala 6',
-          nomeProfessor: 'Professor 6',
+        data: '06/01/2021',
+        nomeSala: 'Sala 6',
+        nomeProfessor: 'Professor 6',
+        info: RelatorioInfo(
           quantidadeAlunosMaximo: 10,
-          quantidadeAlunosPresentes: 5),
+          quantidadeAlunosPresentes: 5,
+          duracaoMinutos: 60,
+          tempoMedioAtendimento: 10,
+          tamanhoMaximoFilaEspera: 5,
+          alunos: Alunos(
+            nome: 'Aluno 6',
+            quantidadeAtendimentos: 1,
+            tempoMedioAtendimento: 10,
+          ),
+        ),
+      ),
       Relatorios(
-          data: '07/01/2021',
-          nomeSala: 'Sala 7',
-          nomeProfessor: 'Professor 7',
+        data: '07/01/2021',
+        nomeSala: 'Sala 7',
+        nomeProfessor: 'Professor 7',
+        info: RelatorioInfo(
           quantidadeAlunosMaximo: 10,
-          quantidadeAlunosPresentes: 5),
+          quantidadeAlunosPresentes: 5,
+          duracaoMinutos: 60,
+          tempoMedioAtendimento: 10,
+          tamanhoMaximoFilaEspera: 5,
+          alunos: Alunos(
+            nome: 'Aluno 7',
+            quantidadeAtendimentos: 1,
+            tempoMedioAtendimento: 10,
+          ),
+        ),
+      ),
       Relatorios(
-          data: '08/01/2021',
-          nomeSala: 'Sala 8',
-          nomeProfessor: 'Professor 8',
+        data: '08/01/2021',
+        nomeSala: 'Sala 8',
+        nomeProfessor: 'Professor 8',
+        info: RelatorioInfo(
           quantidadeAlunosMaximo: 10,
-          quantidadeAlunosPresentes: 5),
+          quantidadeAlunosPresentes: 5,
+          duracaoMinutos: 60,
+          tempoMedioAtendimento: 10,
+          tamanhoMaximoFilaEspera: 5,
+          alunos: Alunos(
+            nome: 'Aluno 8',
+            quantidadeAtendimentos: 1,
+            tempoMedioAtendimento: 10,
+          ),
+        ),
+      ),
       Relatorios(
-          data: '09/01/2021',
-          nomeSala: 'Sala 9',
-          nomeProfessor: 'Professor 9',
+        data: '09/01/2021',
+        nomeSala: 'Sala 9',
+        nomeProfessor: 'Professor 9',
+        info: RelatorioInfo(
           quantidadeAlunosMaximo: 10,
-          quantidadeAlunosPresentes: 5),
+          quantidadeAlunosPresentes: 5,
+          duracaoMinutos: 90,
+          tempoMedioAtendimento: 10,
+          tamanhoMaximoFilaEspera: 5,
+          alunos: Alunos(
+            nome: 'Aluno 9',
+            quantidadeAtendimentos: 1,
+            tempoMedioAtendimento: 10,
+          ),
+        ),
+      ),
       Relatorios(
-          data: '10/01/2021',
-          nomeSala: 'Sala 10',
-          nomeProfessor: 'Professor 10',
+        data: '10/01/2021',
+        nomeSala: 'Sala 10',
+        nomeProfessor: 'Professor 10',
+        info: RelatorioInfo(
           quantidadeAlunosMaximo: 10,
-          quantidadeAlunosPresentes: 5),
+          quantidadeAlunosPresentes: 5,
+          duracaoMinutos: 90,
+          tempoMedioAtendimento: 10,
+          tamanhoMaximoFilaEspera: 5,
+          alunos: Alunos(
+            nome: '',
+            quantidadeAtendimentos: 1,
+            tempoMedioAtendimento: 10,
+          ),
+        ),
+      ),
     ]);
+
+final relatorioSelecionadoProvider = StateProvider<int>((ref) => 0);
 
 class ListaRelatorio extends ConsumerWidget {
   const ListaRelatorio({Key? key}) : super(key: key);
@@ -97,11 +210,16 @@ class ListaRelatorio extends ConsumerWidget {
                       padding: const EdgeInsets.all(12.0),
                       child: TextButton(
                         onPressed: () {
-/*                           Navigator.push(
+                          ref
+                              .read(relatorioSelecionadoProvider.notifier)
+                              .state = index;
+
+                          Navigator.push(
                             context,
                             MaterialPageRoute(
-                                builder: (context) => (const MyApp())),
-                          ); */
+                                builder: (context) =>
+                                    (const RelatorioIndividual())),
+                          );
                         },
                         child: ListTile(
                           shape: RoundedRectangleBorder(
@@ -111,7 +229,7 @@ class ListaRelatorio extends ConsumerWidget {
                           title: Text(relatorio.data),
                           subtitle: Text(relatorio.nomeSala),
                           trailing: Text(
-                              'Alunos: ${relatorio.quantidadeAlunosPresentes}/${relatorio.quantidadeAlunosMaximo}'),
+                              'Alunos: ${relatorio.info.quantidadeAlunosPresentes}/${relatorio.info.quantidadeAlunosMaximo}'),
                         ),
                       ),
                     );
@@ -130,14 +248,42 @@ class Relatorios {
   String data;
   String nomeSala;
   String nomeProfessor;
-  int quantidadeAlunosMaximo;
-  int quantidadeAlunosPresentes;
+  RelatorioInfo info;
 
   Relatorios({
     required this.data,
     required this.nomeSala,
     required this.nomeProfessor,
+    required this.info,
+  });
+}
+
+class RelatorioInfo {
+  int quantidadeAlunosMaximo;
+  int quantidadeAlunosPresentes;
+  int duracaoMinutos;
+  int tempoMedioAtendimento;
+  int tamanhoMaximoFilaEspera;
+  Alunos alunos;
+
+  RelatorioInfo({
     required this.quantidadeAlunosMaximo,
     required this.quantidadeAlunosPresentes,
+    required this.duracaoMinutos,
+    required this.tempoMedioAtendimento,
+    required this.tamanhoMaximoFilaEspera,
+    required this.alunos,
+  });
+}
+
+class Alunos {
+  String nome;
+  int quantidadeAtendimentos;
+  int tempoMedioAtendimento;
+
+  Alunos({
+    required this.nome,
+    required this.quantidadeAtendimentos,
+    required this.tempoMedioAtendimento,
   });
 }
